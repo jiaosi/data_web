5bdf8e6e7ca6b47a34214f4a99e92093996516df
jiaosi_glb <jiaosi_glb@fqy.com>
2022-12-05 10:21:59 +0800

    adjust plot

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
 import db
 from io import BytesIO
 import base64
@@ -15,28 +16,35 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     sql = "select * from mytest_db.house_trade_data where str_to_date(date, '%Y/%m/%d') between date_sub(now(), interval 60 day) and now();"
-    na = ()
     mydb = db.DB()
-    results = mydb.get_record(sql, na)
+    data = pd.read_sql(sql, mydb.db)
     mydb.close()
-    results.reverse()
-    d_array = []
-    for result in results:
-        d_array.append([result[4], result[7], result[9]])
-    data = pd.DataFrame(d_array)
-    data.columns = ['new', 'old', 'date']
-    data['date'] = pd.to_datetime(data['date'], format = '%Y/%m/%d')
+    data['date'] = pd.to_datetime(data['date'], format='%Y/%m/%d')
     data = data.sort_values('date')
 
     fig, ax = plt.subplots(figsize=(20, 6))
     # data.fillna(0, inplace = True)
-    data['new'] = data['new'].astype('int')
-    data['old'] = data['old'].astype('int')
+    data['new_residence_num'] = data['new_residence_num'].astype('int')
+    data['old_residence_num'] = data['old_residence_num'].astype('int')
     ax.set_xlim(data['date'].values[-1], data['date'].values[0])
-    ax.plot(data['date'], data['new'], label='new')
-    ax.plot(data['date'], data['old'], label='old')
-    plt.xticks(rotation = 30)
+    ax.bar(data['date'], data['new_residence_num'], label='new_residence_num')
+    ax.bar(data['date'], data['old_residence_num'], label='old_residence_num', alpha=0.5)
+    plt.legend(loc='upper left')
+    plt.xticks(rotation=30)
+
+    ax2 = ax.twinx()
+    ax2.plot(data['date'],
+             data[['new_residence_area', 'date']].apply(lambda x: np.nan if x[1].weekday() in (5, 6) else float(x[0]),
+                                                        axis=1)
+             / data['new_residence_num'], label='average_new_area',
+             linestyle='-.')
+    ax2.plot(data['date'],
+             data[['old_residence_area', 'date']].apply(lambda x: np.nan if x[1].weekday() in (5, 6) else float(x[0]),
+                                                        axis=1)
+             / data['old_residence_num'], label='average_old_area',
+             linestyle='-.')
     plt.legend()
+
 
     bio = BytesIO()
     plt.savefig(bio, format = 'png')
@@ -68,5 +76,5 @@ def show_subpath(subpath):
 # if __name__ == '__main__':
 #     app.run(debug=True,host='0.0.0.0')
 
-server = make_server("", 6001, app)
-server.serve_forever()  # poll_interval轮询时间
+# server = make_server("", 6001, app)
+# server.serve_forever()  # poll_interval轮询时间
